0e58d0aecfa04205e0ee42cc6d8069421ed2c7bd
nekomancer0 <nekomancers0@gmail.com>
2024-08-22 12:41:39 +0200

    first commit

--- index.js
+++ index.js
@@ -1,121 +1,74 @@
-var context = document.querySelector(".context");
-var textarea = document.querySelector("textarea");
+"use strict";
+let context = document.querySelector(".context");
+let color = document.querySelector("#color");
 //@ts-ignore
-var game = new BoardMaker({
-    boxNames: ["blue", "red", "wall", "empty", "power", "must"],
+let game = new BoardMaker({
     height: localStorage.getItem("height") || 10,
     width: localStorage.getItem("width") || 10,
-    context: context,
+    context,
 });
-game.init();
-setTimeout(function () {
-    var metadatas = localStorage.getItem("board-data")
-        ? JSON.parse(localStorage.getItem("board-data"))
-        : [];
-    metadatas.forEach(function (data) {
-        var _a = data.position, x = _a.x, y = _a.y;
-        var item = game.getElement({ x: x, y: y });
-        item.classList.add(data.name);
-    });
-}, 200);
-var buttonGet = document.querySelector(".get");
-var buttonSet = document.querySelector(".set");
-var heightInp = document.querySelector("#height");
-var widthInp = document.querySelector("#width");
-var cols = document.querySelectorAll(".col");
+let contextCopy = document.createElement("div");
+contextCopy.classList.add("context");
+contextCopy.innerHTML = context.innerHTML;
+let heightInp = document.querySelector("#height");
+let widthInp = document.querySelector("#width");
+let cols = document.querySelectorAll(".col");
+let switchBtn = document.querySelector("#switch");
+let rowHistory = [];
 listen();
+//@ts-ignore
+switchBtn.addEventListener("click", (ev) => {
+    if (ev.target.classList.contains("edit-mode")) {
+        //@ts-ignore
+        html2canvas(context, { backgroundColor: null }).then(function (canvas) {
+            const img = canvas.toDataURL("image/png");
+            context.innerHTML = '<img src="' + img + '"/>';
+        });
+        ev.target.classList.remove("edit-mode");
+        ev.target.classList.add("image-mode");
+        ev.target.textContent = "Image Mode";
+    }
+    else {
+        game.init();
+        context.innerHTML = contextCopy.innerHTML;
+        ev.target.classList.remove("image-mode");
+        ev.target.classList.add("edit-mode");
+        ev.target.textContent = "Edit Mode";
+    }
+});
 function listen() {
     cols = document.querySelectorAll(".col");
     if (localStorage.getItem("width"))
         widthInp.value = localStorage.getItem("width");
     if (localStorage.getItem("height"))
         heightInp.value = localStorage.getItem("height");
-    var selectedName = "empty";
-    var lastItem = null;
-    cols.forEach(function (col) {
-        var _loop_1 = function (row) {
-            var classes = Array.from(row.classList);
+    cols.forEach((col) => {
+        for (let row of col.children) {
             //@ts-ignore
-            row.addEventListener("click", function (ev) {
-                var _a = game.getPosition(row), x = _a.x, y = _a.y;
-                var classes = Array.from(row.classList);
-                var lastName = classes.at(-1);
-                if (lastName === selectedName) {
-                    row.classList.remove(lastName);
-                    row.classList.add("empty");
-                    if (lastItem === game.getElemet({ x: x, y: y }))
-                        game.removeData({ x: x, y: y });
+            row.addEventListener("click", (ev) => {
+                let { x, y } = game.getPosition(ev.target);
+                rowHistory.push({ x, y });
+                if (color.value === "#ffffff") {
+                    ev.target.style.backgroundColor = "transparent";
+                    return;
                 }
-                else {
-                    if (lastName.startsWith("item-")) {
-                        row.classList.add(selectedName);
-                        // game.addData({
-                        //   position: { x, y },
-                        //   name: selectedName,
-                        // });
-                    }
-                    else {
-                        row.classList.remove(lastName);
-                        row.classList.add(selectedName);
-                        // game.addData({
-                        //   position: { x, y },
-                        //   name: selectedName,
-                        // });
-                    }
-                }
+                ev.target.style.backgroundColor = color.value;
             });
-        };
-        for (var _i = 0, _a = col.children; _i < _a.length; _i++) {
-            var row = _a[_i];
-            _loop_1(row);
         }
     });
-    var boxNames = document.querySelector(".boxNames");
-    for (var _i = 0, _a = boxNames === null || boxNames === void 0 ? void 0 : boxNames.children; _i < _a.length; _i++) {
-        var a = _a[_i];
-        // @ts-ignore
-        a.addEventListener("click", function (ev) {
-            ev.preventDefault();
-            var name = ev.target.textContent;
-            ev.target.classList.add("targeted");
-            var allA = Array.from(boxNames.children).filter(function (a) { return a.textContent !== name; });
-            allA.forEach(function (a) {
-                a.classList.remove("targeted");
-            });
-            selectedName = name;
-        });
-    }
-    buttonGet.addEventListener("click", function (ev) {
-        textarea.value = JSON.stringify(game.toMetadatas());
-        context = document.querySelector(".context");
-        game.context = context;
-        localStorage.setItem("board-data", textarea.value);
-        listen();
-    });
-    buttonSet.addEventListener("click", function (ev) {
-        var metadatas = JSON.parse(textarea.value);
-        game.metadatas = metadatas;
-        game.init();
-        game.metadatas.forEach(function (data) {
-            var _a = data.position, x = _a.x, y = _a.y, name = data.name;
-            var item = game.getElement({ x: x, y: y });
-            item.classList.add(name);
-        });
-        listen();
-    });
 }
-heightInp === null || heightInp === void 0 ? void 0 : heightInp.addEventListener("change", 
+heightInp.addEventListener("change", 
 //@ts-ignore
-function (ev) {
+(ev) => {
     game.height = parseInt(ev.target.value);
     game.init();
     cols = document.querySelectorAll(".col");
     localStorage.setItem("height", game.height);
     listen();
 });
-widthInp === null || widthInp === void 0 ? void 0 : widthInp.addEventListener("change", 
+widthInp.addEventListener("change", 
 //@ts-ignore
-function (ev) {
+(ev) => {
     game.width = parseInt(ev.target.value);
     localStorage.setItem("width", game.width);
     game.init();
--- index.ts
+++ index.ts
@@ -1,41 +1,50 @@
 let context: HTMLDivElement = document.querySelector(".context")!;
 
-let textarea = document.querySelector("textarea")!;
+let color = document.querySelector<HTMLInputElement>("#color")!;
 //@ts-ignore
 let game = new BoardMaker({
-  boxNames: ["blue", "red", "wall", "empty", "power", "must"],
   height: localStorage.getItem("height") || 10,
   width: localStorage.getItem("width") || 10,
   context,
 });
 
-game.init();
-
-setTimeout(() => {
-  let metadatas = localStorage.getItem("board-data")
-    ? JSON.parse(localStorage.getItem("board-data")!)
-    : [];
-
-  metadatas.forEach(
-    (data: { position: { x: number; y: number }; name: string }) => {
-      let {
-        position: { x, y },
-      } = data;
-
-      let item = game.getElement({ x, y });
-      item.classList.add(data.name);
-    }
-  );
-}, 200);
-
-let buttonGet: HTMLButtonElement = document.querySelector(".get")!;
-let buttonSet: HTMLButtonElement = document.querySelector(".set")!;
+let contextCopy = document.createElement("div");
+contextCopy.classList.add("context");
+contextCopy.innerHTML = context.innerHTML;
 
 let heightInp = document.querySelector<HTMLInputElement>("#height")!;
 let widthInp = document.querySelector<HTMLInputElement>("#width")!;
 let cols = document.querySelectorAll<HTMLDivElement>(".col")!;
+let switchBtn = document.querySelector<HTMLButtonElement>("#switch")!;
+
+let rowHistory: { x: number; y: number }[] = [];
 
 listen();
+
+//@ts-ignore
+switchBtn.addEventListener("click", (ev: { target: HTMLButtonElement }) => {
+  if (ev.target.classList.contains("edit-mode")) {
+    //@ts-ignore
+    html2canvas(context, { backgroundColor: null }).then(function (
+      canvas: HTMLCanvasElement
+    ) {
+      const img = canvas.toDataURL("image/png");
+      context.innerHTML = '<img src="' + img + '"/>';
+    });
+
+    ev.target.classList.remove("edit-mode");
+    ev.target.classList.add("image-mode");
+    ev.target.textContent = "Image Mode";
+  } else {
+    game.init();
+
+    context.innerHTML = contextCopy.innerHTML;
+
+    ev.target.classList.remove("image-mode");
+    ev.target.classList.add("edit-mode");
+    ev.target.textContent = "Edit Mode";
+  }
+});
 
 function listen() {
   cols = document.querySelectorAll<HTMLDivElement>(".col")!;
@@ -46,104 +55,27 @@ function listen() {
   if (localStorage.getItem("height"))
     heightInp.value = localStorage.getItem("height")!;
 
-  let selectedName = "empty";
-  let lastItem: HTMLDivElement | null = null;
-
   cols.forEach((col) => {
     for (let row of col.children) {
-      let classes = Array.from(row.classList);
-
       //@ts-ignore
       row.addEventListener("click", (ev: { target: HTMLDivElement }) => {
-        let { x, y } = game.getPosition(row);
-        let classes = Array.from(row.classList);
+        let { x, y } = game.getPosition(ev.target);
 
-        let lastName = classes.at(-1)!;
+        rowHistory.push({ x, y });
 
-        if (lastName === selectedName) {
-          row.classList.remove(lastName);
-          row.classList.add("empty");
+        if (color.value === "#ffffff") {
+          ev.target.style.backgroundColor = "transparent";
 
-          if (lastItem === game.getElemet({ x, y })) game.removeData({ x, y });
-        } else {
-          if (lastName.startsWith("item-")) {
-            row.classList.add(selectedName);
-
-            // game.addData({
-            //   position: { x, y },
-            //   name: selectedName,
-            // });
-          } else {
-            row.classList.remove(lastName);
-            row.classList.add(selectedName);
-
-            // game.addData({
-            //   position: { x, y },
-            //   name: selectedName,
-            // });
-          }
+          return;
         }
+
+        ev.target.style.backgroundColor = color.value;
       });
     }
   });
-
-  let boxNames = document.querySelector(".boxNames")!;
-
-  for (let a of boxNames?.children) {
-    // @ts-ignore
-    a.addEventListener("click", (ev: Event & { target: HTMLLinkElement }) => {
-      ev.preventDefault();
-
-      let name = ev.target.textContent!;
-
-      ev.target.classList.add("targeted");
-
-      let allA = Array.from(boxNames.children).filter(
-        (a) => a.textContent! !== name
-      );
-
-      allA.forEach((a) => {
-        a.classList.remove("targeted");
-      });
-
-      selectedName = name;
-    });
-  }
-
-  buttonGet.addEventListener("click", (ev) => {
-    textarea.value = JSON.stringify(game.toMetadatas());
-
-    context = document.querySelector<HTMLDivElement>(".context")!;
-
-    game.context = context;
-
-    localStorage.setItem("board-data", textarea.value);
-
-    listen();
-  });
-
-  buttonSet.addEventListener("click", (ev) => {
-    let metadatas = JSON.parse(textarea.value);
-
-    game.metadatas = metadatas;
-
-    game.init();
-
-    game.metadatas.forEach((data: any) => {
-      let {
-        position: { x, y },
-        name,
-      } = data;
-
-      let item = game.getElement({ x, y });
-      item.classList.add(name);
-    });
-
-    listen();
-  });
 }
 
-heightInp?.addEventListener(
+heightInp.addEventListener(
   "change",
 
   //@ts-ignore
@@ -158,7 +90,7 @@ heightInp?.addEventListener(
   }
 );
 
-widthInp?.addEventListener(
+widthInp.addEventListener(
   "change",
 
   //@ts-ignore
