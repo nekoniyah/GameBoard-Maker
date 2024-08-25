e85217c75a6261a654d5fbf94b2ab5dab42d04cd
nekomancer0 <nekomancers0@gmail.com>
2024-08-25 17:20:10 +0200

    Fixed and updated

--- index.js
+++ index.js
@@ -1,120 +1,124 @@
-"use strict";
-let context = document.querySelector(".context");
-let color = document.querySelector("#color");
-//@ts-ignore
-let game = new BoardMaker({
-    height: localStorage.getItem("height") || 10,
-    width: localStorage.getItem("width") || 10,
-    context,
-});
-let contextCopy = document.createElement("div");
+// index.ts
+function listen() {
+  let down = false;
+  cols = document.querySelectorAll(".col");
+  context = document.querySelector(".context");
+  if (localStorage.getItem("width"))
+    widthInp.value = localStorage.getItem("width");
+  if (localStorage.getItem("height"))
+    heightInp.value = localStorage.getItem("height");
+  cols.forEach((col) => {
+    let children = Array.from(col.children);
+    for (let row of children) {
+      let click2 = function(ev) {
+        let { x, y } = game.getPosition(ev.target);
+        let colors = JSON.parse(localStorage.getItem("colors"));
+        let val = color.value === "#ffffff" ? "transparent" : color.value;
+        ev.target.style.backgroundColor = val;
+        rowHistory = [{ x, y, color: "transparent" }, ...rowHistory];
+        if (!colors.includes(val)) {
+          colors = [...colors, val];
+          window.localStorage.setItem("colors", JSON.stringify(colors));
+          let div = document.createElement("div");
+          div.classList.add("color");
+          div.style.backgroundColor = val;
+          lastColorsDiv.appendChild(div);
+        }
+      };
+      var click = click2;
+      row.addEventListener("mousedown", (ev) => {
+        down = true;
+      });
+      row.addEventListener("mouseup", (ev) => {
+        down = false;
+      });
+      row.addEventListener("mouseenter", (ev) => {
+        ev.target.classList.add("hovered");
+        if (down) {
+          click2(ev);
+        }
+      });
+      row.addEventListener("click", click2);
+      row.addEventListener("mouseleave", (ev) => {
+        ev.target.classList.remove("hovered");
+      });
+    }
+  });
+}
+var context = document.querySelector(".context");
+var color = document.querySelector("#color");
+var game = new BoardMaker(context, localStorage.getItem("height") ? parseInt(localStorage.getItem("height")) : 10, localStorage.getItem("width") ? parseInt(localStorage.getItem("width")) : 10);
+game.init();
+var contextCopy = document.createElement("div");
 contextCopy.classList.add("context");
 contextCopy.innerHTML = context.innerHTML;
-let heightInp = document.querySelector("#height");
-let widthInp = document.querySelector("#width");
-let cols = document.querySelectorAll(".col");
-let switchBtn = document.querySelector("#switch");
-let clearBtn = document.querySelector("#clear");
-let undoBtn = document.querySelector("#undo");
-let rowHistory = [];
+var heightInp = document.querySelector("#height");
+var widthInp = document.querySelector("#width");
+var cols = document.querySelectorAll(".col");
+var switchBtn = document.querySelector("#switch");
+var clearBtn = document.querySelector("#clear");
+var undoBtn = document.querySelector("#undo");
+var lastColorsDiv = document.querySelector(".last-colors");
+var rowHistory = [];
+lastColorsDiv.innerHTML = "";
+if (localStorage.getItem("colors")) {
+  let colors = JSON.parse(window.localStorage.getItem("colors"));
+  colors.forEach((color2) => {
+    let div = document.createElement("div");
+    div.classList.add("color");
+    div.style.backgroundColor = color2;
+    lastColorsDiv.appendChild(div);
+  });
+} else {
+  localStorage.setItem("colors", JSON.stringify([]));
+}
 listen();
-//@ts-ignore
 switchBtn.addEventListener("click", (ev) => {
-    if (ev.target.classList.contains("edit-mode")) {
-        //@ts-ignore
-        html2canvas(context, { backgroundColor: null }).then(function (canvas) {
-            const img = canvas.toDataURL("image/png");
-            context.innerHTML = '<img src="' + img + '"/>';
-        });
-        listen();
-        ev.target.classList.remove("edit-mode");
-        ev.target.classList.add("image-mode");
-        ev.target.textContent = "Image Mode";
-    }
-    else {
-        context.innerHTML = "";
-        context.innerHTML = contextCopy.innerHTML;
-        ev.target.classList.remove("image-mode");
-        ev.target.classList.add("edit-mode");
-        ev.target.textContent = "Edit Mode";
-        listen();
-    }
+  if (ev.target.classList.contains("edit-mode")) {
+    html2canvas(context, { backgroundColor: null }).then(function(canvas) {
+      const img = canvas.toDataURL("image/png");
+      context.innerHTML = '<img src="' + img + '"/>';
+    });
+    listen();
+    ev.target.classList.remove("edit-mode");
+    ev.target.classList.add("image-mode");
+    ev.target.textContent = "Image Mode";
+  } else {
+    context.innerHTML = "";
+    context.innerHTML = contextCopy.innerHTML;
+    ev.target.classList.remove("image-mode");
+    ev.target.classList.add("edit-mode");
+    ev.target.textContent = "Edit Mode";
+    listen();
+  }
 });
 undoBtn.addEventListener("click", (ev) => {
-    let el = game.getElement(rowHistory[0]);
-    if (rowHistory.length === 1) {
-        rowHistory = [];
-    }
-    else if (rowHistory.length === 0)
-        return;
-    else {
-        let recent = rowHistory.shift();
-    }
-    el.style.backgroundColor = "transparent";
-});
-function listen() {
-    let down = false;
-    cols = document.querySelectorAll(".col");
-    context = document.querySelector(".context");
-    if (localStorage.getItem("width"))
-        widthInp.value = localStorage.getItem("width");
-    if (localStorage.getItem("height"))
-        heightInp.value = localStorage.getItem("height");
-    cols.forEach((col) => {
-        for (let row of col.children) {
-            row.addEventListener("mousedown", (ev) => {
-                down = true;
-            });
-            row.addEventListener("mouseup", (ev) => {
-                down = false;
-            });
-            function click(ev) {
-                let { x, y } = game.getPosition(ev.target);
-                if (color.value === "#ffffff") {
-                    ev.target.style.backgroundColor = "transparent";
-                    rowHistory = [{ x, y, color: "transparent" }, ...rowHistory];
-                    return;
-                }
-                ev.target.style.backgroundColor = color.value;
-                rowHistory = [{ x, y, color: color.value }, ...rowHistory];
-                console.log(rowHistory);
-            }
-            //@ts-ignore
-            row.addEventListener("mouseenter", (ev) => {
-                ev.target.classList.add("hovered");
-                if (down) {
-                    click(ev);
-                }
-            });
-            //@ts-ignore
-            row.addEventListener("click", click);
-            //@ts-ignore
-            row.addEventListener("mouseleave", (ev) => {
-                ev.target.classList.remove("hovered");
-            });
-        }
-    });
-}
-clearBtn.addEventListener("click", (ev) => {
+  let el = game.getElement(rowHistory[0]);
+  if (rowHistory.length === 1) {
     rowHistory = [];
-    game.init();
-    listen();
+  } else if (rowHistory.length === 0)
+    return;
+  else {
+    let recent = rowHistory.shift();
+  }
+  el.style.backgroundColor = "transparent";
 });
-heightInp.addEventListener("change", 
-//@ts-ignore
-(ev) => {
-    game.height = parseInt(ev.target.value);
-    game.init();
-    cols = document.querySelectorAll(".col");
-    localStorage.setItem("height", game.height);
-    listen();
+clearBtn.addEventListener("click", (ev) => {
+  rowHistory = [];
+  game.init();
+  listen();
 });
-widthInp.addEventListener("change", 
-//@ts-ignore
-(ev) => {
-    game.width = parseInt(ev.target.value);
-    localStorage.setItem("width", game.width);
-    game.init();
-    cols = document.querySelectorAll(".col");
-    listen();
+heightInp.addEventListener("change", (ev) => {
+  game.height = parseInt(ev.target.value);
+  game.init();
+  cols = document.querySelectorAll(".col");
+  localStorage.setItem("height", `${game.height}`);
+  listen();
+});
+widthInp.addEventListener("change", (ev) => {
+  game.width = parseInt(ev.target.value);
+  localStorage.setItem("width", `${game.width}`);
+  game.init();
+  cols = document.querySelectorAll(".col");
+  listen();
 });
